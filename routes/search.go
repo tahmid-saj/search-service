75108098d3b2467eaf80208d127c094a8cf49d84
tahmid-saj <tahmid.saj@gmail.com>
2024-09-27 17:21:57 -0400

    trie / search routes and logic

--- routes/search.go
+++ routes/search.go
@@ -1,15 +1,62 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"search-service/models"
+
+	"github.com/gin-gonic/gin"
+)
 
 func search(context *gin.Context) {
-	
+	var searchQueryInput models.SearchQueryInput
+
+	err := context.ShouldBindJSON(&searchQueryInput)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request body"})
+		return
+	}
+
+	searchResults, err := models.Search(searchQueryInput)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch search results"})
+		return
+	}
+
+	context.JSON(http.StatusOK, searchResults)
 }
 
 func addSearchQuery(context *gin.Context) {
+	var searchQueryInput models.SearchQueryInput
 
+	err := context.ShouldBindJSON(&searchQueryInput)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request body"})
+		return
+	}
+
+	searchQueryAdded, err := models.AddSearchQuery(searchQueryInput)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not add search query"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, searchQueryAdded)
 }
 
 func deleteSearchQuery(context *gin.Context) {
+	var searchQueryInput models.SearchQueryInput
 
+	err := context.ShouldBindJSON(&searchQueryInput)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request body"})
+		return
+	}
+
+	searchQueryDeleted, err := models.DeleteSearchQuery(searchQueryInput)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "could not delete search query"})
+		return
+	}
+
+	context.JSON(http.StatusOK, searchQueryDeleted)
 }
